3c7b1769a4903cb452458e08e1de769ad9bddefd
Lorenzo Behna <lorenzo.behna@gmail.com>
2022-10-26 21:42:24 +0200

    Main was still changed for testing(); Now reverted back to "default".

--- Group39-a1.zip/src/Main.java
+++ Group39-a1.zip/src/Main.java
@@ -1,11 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        Player hoom = new HumanPlayer();
-        hoom.shoot();
-
-        //Game game = new Game();
-        //game.initGame();
+        Game game = new Game();
+        game.initGame();
     }
 
 }
